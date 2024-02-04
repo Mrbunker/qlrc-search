7142062f97cbd6b9fe52001893c028a6d56050f7
Mrbunker <2238944686@qq.com>
2024-02-04 14:46:13 +0800

    Missing Suspense boundary with useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import MusicSearch from "./components/MusicSearch";
 import PageContainer from "./components/PageContainer";
 
 export default function Home() {
   return (
     <PageContainer>
-      <MusicSearch />
+      <Suspense fallback={<div>Loading...</div>}>
+        <MusicSearch />
+      </Suspense>
     </PageContainer>
   );
 }
