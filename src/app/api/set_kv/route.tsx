cfc3561d441cfc2555f6a8bacd4906e38f02fbdf
Mrbunker <2238944686@qq.com>
2024-02-05 15:01:11 +0800

    kv api

---
+++ src/app/api/set_kv/route.tsx
@@ -0,0 +1,16 @@
+import { kv } from "@vercel/kv";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const key = searchParams.get("key");
+  const value = searchParams.get("value");
+  console.log("|key", key);
+  if (!key || !value) {
+    return Response.json(
+      { code: 400, error: "key and value are required" },
+      { status: 400 }
+    );
+  }
+  await kv.set(key, value);
+  return Response.json({ code: 0 }, { status: 200 });
+}
